6ad84e493555b461324cd6805bbf9fa8b6c6c16c
Matthew Badali <Matthew Badali>
2016-04-11 02:21:45 -0500

    1

--- Sample.php
+++ Sample.php
@@ -38,8 +38,9 @@
 			echo "Setting target temperatures ($ctemp) for $serial... \r\n";
 			//$success = $nest->setTargetTemperature($ctemp,"02AA01AC311303CZ");
 			//var_dump($success);
-			$serial_number = $this->getDefaultSerial($serial_number);
-			echo $serial_number;
+			$infos = $nest->getDeviceInfo($devices_serials[0]);	
+			//$serial_number = $this->getDefaultSerial($serial_number);
+			print_r($infos);
 				var_dump($success);
 		}
 		unset($serial);
